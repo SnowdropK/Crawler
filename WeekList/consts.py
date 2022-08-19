7f6b3cce6d56e66442db91d8fa90032c0047b8b8
宇智波胖虎 <39355424+SnowdropK@users.noreply.github.com>
2022-08-19 14:39:16 +0800

    Update consts.py

--- WeekList/consts.py
+++ WeekList/consts.py
@@ -7,7 +7,4 @@ Xianyan = "https://www.jjwxc.net/bookbase.php?fw1=1&fbsj2=2&ycx1=1&xx1=1&mainvie
 originNovelUrl = "https://www.jjwxc.net/onebook.php?novelid="
 
 # 登录信息
-Cookie = "CNZZDATA30075907=cnzz_eid%3D33291830-1649564210-null%26ntime%3D1651499524; timeOffset_o=286.300048828125; " \
-         "__yjs_duid=1_374432dce397ee8803f418876f9e3d431659887108768; testcookie=yes; " \
-         "Hm_lvt_bc3b748c21fe5cf393d26c12b2c38d99=1659437058,1659882131,1659887087,1659887109; " \
-         "token=MTQ0ODIxNDN8YWFhODAwOThlOTRhOWQ1ZDliNjdlM2Q4ODFlNjZhOTZ8fHNodSoqKioqKioqQDE2My5jb218MjIyMTQxNnw0MzIwMHwxfOaoseepuuazqnx85pmL5rGf55So5oi3fDF8ZW1haWx8MXwwfHw%3D; JJSESS=%7B%22returnUrl%22%3A%22https%3A//www.jjwxc.net/%22%2C%22clicktype%22%3A%22%22%7D; JJEVER=%7B%22fenzhan%22%3A%22noyq%22%2C%22nicknameAndsign%22%3A%222%257E%2529%2524%25E6%25B5%25B7%25E9%25A3%258E%25E4%25B9%258B%25E8%25A1%2597%22%2C%22foreverreader%22%3A%2214482143%22%2C%22desid%22%3A%2212jo+lRSTSXmWKg70VJCujw0ZFhMBMQb%22%2C%22sms_total%22%3A%223%22%2C%22lastCheckLoginTimePc%22%3A%221659884531%22%2C%22background%22%3A%22%22%2C%22font_size%22%3A%22%22%2C%22isKindle%22%3A%22%22%7D; smidV2=20220410150200d27cb558332fcea931399022d9117e1b00f9664806e9d3880; Hm_lpvt_bc3b748c21fe5cf393d26c12b2c38d99=1659887216 "
+Cookie = ""
